c008d65c2cf97dcfba552d2a3c2fbd8c42233055
titov-andrei <titov_andrei@mail.ru>
2022-02-03 17:53:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function App() {
+  let numbers = [1, 555, 3];
   return (
     <div className="App">
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        {numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
       </ul>
       <button>Новое число</button>
     </div>
-  )
+  );
 }
 
 export default App;
